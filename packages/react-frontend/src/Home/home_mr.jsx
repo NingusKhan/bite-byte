1cb4ba72e6992fa10d1a3f61f1afaeaded1fe6fe
kyyyyle <kyleschoolstuffq@gmail.com>
2024-12-02 13:56:49 -0800

    flavor text

--- packages/react-frontend/src/Home/home_mr.jsx
+++ packages/react-frontend/src/Home/home_mr.jsx
@@ -5,7 +5,7 @@ function HomeMR() {
         <div class="row">
             <div id="column-2" class="col-sm-6">
                 <h3>My Recipes</h3>
-                <p>See your recipes here!</p>
+                <p>See your recipes here! Use this button to go to the My Recipes tab, where you add your own recipes, along with images and ingredients. You can also remove recipes if you decide that you no longer want them saved.</p>
                 <a href="myrecipes" class="btn" id="button">See More...</a>
             </div>
             <div id="myrecipes" class="col-sm-6"></div>
