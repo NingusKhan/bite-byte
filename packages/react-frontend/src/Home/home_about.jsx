d75d95159a2d1f402d2f7b88953583fa4bc62472
hudsonschmidt <hudsonschmidt08@gmail.com>
2024-12-04 13:49:07 -0800

    added Kyle's blurbs for home page

--- packages/react-frontend/src/Home/home_about.jsx
+++ packages/react-frontend/src/Home/home_about.jsx
@@ -5,7 +5,7 @@ function HomeAbout() {
     <div class="row">
         <div id="column-0" class="col-sm-12">
             <h3>About Us</h3>
-            <p>Write about our project here</p>
+            <p>Bite & Byte is a group formed for a class project that wanted to create a product that allows users to easily search for recipes and the ingredients needed to make cooking as seamless as possible. Future plans for this project include an AI system that the user could use to scan their refrigerator for ingredients in order to give recipe suggestions. This would save the user time and prevent food waste.</p>
         </div>
     </div>
   );
--- packages/react-frontend/src/Home/home_browse.jsx
+++ packages/react-frontend/src/Home/home_browse.jsx
@@ -6,7 +6,7 @@ function HomeBrowse() {
         <div id="browse" class="col-sm-6"></div>
         <div id="column-2" class="col-sm-6">
             <h3>Browse</h3>
-            <p>Find recipes here!</p>
+            <p>Find recipes here! Use this button to go to the browse tab, where can search for specific recipes listed. If you find one you like, you can favorite the recipe, which will save it to My Recipes.</p>
             <a href="browse" class="btn" id="button">See More...</a>
         </div>
     </div>
